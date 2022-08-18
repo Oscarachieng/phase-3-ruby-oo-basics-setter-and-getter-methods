35cca26ce10434d284b5263228cc8a17ed2cf326
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-18 19:43:16 +0300

    commit tree 4992bf993fe04e4f67f42ede7d3f44c049534d59

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
+   #defining the setter method
+   def name= (dog_name)
+      @name = dog_name
+   end
+   #getter method
+   def name
+       @name
+   end
+    
+   #breed setter
+   def breed=(dog_breed)
+     @breed = dog_breed
+   end
+   #breed getter
+   def breed
+     @breed
+   end
 
 end
+
+liza = Dog.new
+liza.name = "enox"
+puts liza.name
