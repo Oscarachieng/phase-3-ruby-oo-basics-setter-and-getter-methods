833076994f4aa7f62c23c8a708c45e9839373e55
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-18 19:43:24 +0300

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
+    #name setter
+    def name= (my_name)
+        @name = my_name
+    end
+    #name getter
+    def name
+        @name
+    end
+    #job setter
+     
+    def job= (work)
+        @job = work
+    end
+     
+    #job getter
+    def job
+        @job
+    end
+
 
 end
